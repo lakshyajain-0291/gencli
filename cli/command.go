3535449abea9bbd83ba7292aac02e8610a2dc30e
Lakshya Jain <b23cs1032@iitj.ac.in>
2024-07-15 07:47:45 +0530

    added flags in config cmd for show, add and delete

--- cli/command.go
+++ cli/command.go
@@ -1,29 +1,48 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 var opts setupOpts
 
 func NewConfigCommand() *cobra.Command {
-	var directories []string
-	var skipTypes []string
-	var skipFiles []string
+	var addDirectories []string
+	var deleteDirectories []string
+	var addSkipTypes []string
+	var deleteSkipTypes []string
+	var addSkipFiles []string
+	var deleteSkipFiles []string
 	var relevanceIndex float32
+	var showConfig bool
 
 	cmd := &cobra.Command{
 		Use:   "config",
 		Short: "Set configurations for the indexing",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return setConfig(directories, skipTypes, skipFiles, relevanceIndex)
+			if showConfig {
+				config, err := LoadConfig()
+				if err != nil {
+					return fmt.Errorf("failed to load config: %w", err)
+				}
+
+				showConfigFormatted(config)
+				return nil
+			}
+			return setConfig(addDirectories, deleteDirectories, addSkipTypes, deleteSkipTypes, addSkipFiles, deleteSkipFiles, relevanceIndex)
 		},
 	}
 
-	cmd.Flags().StringSliceVarP(&directories, "directories", "d", []string{}, "List of directories to index")
-	cmd.Flags().StringSliceVarP(&skipTypes, "skiptypes", "t", []string{}, "List of file types to skip during indexing")
-	cmd.Flags().StringSliceVarP(&skipFiles, "skipfiles", "f", []string{}, "List of directories to index")
-	cmd.Flags().Float32VarP(&relevanceIndex, "relativeindex", "r", 0.8, "Relevance Value used during Indexing")
+	cmd.Flags().StringSliceVar(&addDirectories, "add-dir", []string{}, "List of directories to add to the index")
+	cmd.Flags().StringSliceVar(&deleteDirectories, "del-dir", []string{}, "List of directories to remove from the index")
+	cmd.Flags().StringSliceVar(&addSkipTypes, "add-skiptypes", []string{}, "List of file types to skip during indexing")
+	cmd.Flags().StringSliceVar(&deleteSkipTypes, "del-skiptypes", []string{}, "List of file types to stop skipping during indexing")
+	cmd.Flags().StringSliceVar(&addSkipFiles, "add-skipfiles", []string{}, "List of files to skip during indexing")
+	cmd.Flags().StringSliceVar(&deleteSkipFiles, "del-skipfiles", []string{}, "List of files to stop skipping during indexing")
+	cmd.Flags().Float32VarP(&relevanceIndex, "relindex", "r", 0.8, "Relevance Value used during Indexing")
+	cmd.Flags().BoolVarP(&showConfig, "show-config", "s", false, "Show the current configuration")
 
 	return cmd
 }
--- cli/config.go
+++ cli/config.go
@@ -15,18 +15,18 @@ type ConfigData struct {
 	RelevanceIndex float32  `json:"relevance_index"`
 }
 
-func setConfig(directories []string, skipTypes []string, skipFiles []string, relIndex float32) error {
+func setConfig(addDirectories []string, deleteDirectories []string, addSkipTypes []string, deleteSkipTypes []string, addSkipFiles []string, deleteSkipFiles []string, relevanceIndex float32) error {
 
 	config, err := LoadConfig()
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			// if no directories are provided adds current working directory (can also change this to root directory of system)
-			if len(directories) == 0 {
+			// if no addDirectories are provided adds current working directory (can also change this to root directory of system)
+			if len(addDirectories) == 0 {
 				cwd, err := os.Getwd()
 				if err != nil {
 					return fmt.Errorf("failed to get working directory : %w", err)
 				}
-				directories = append(directories, cwd)
+				addDirectories = append(addDirectories, cwd)
 			}
 
 			config = &ConfigData{}
@@ -36,16 +36,32 @@ func setConfig(directories []string, skipTypes []string, skipFiles []string, rel
 		}
 	}
 
-	if len(directories) > 0 {
-		config.Directories = directories
+	if len(addDirectories) > 0 {
+		config.Directories = append(config.Directories, addDirectories...)
 	}
 
-	config.SkipType = skipTypes
+	for _, dir := range deleteDirectories {
+		config.Directories = removeElements(config.Directories, dir)
+	}
 
-	config.SkipFile = skipFiles
+	if len(addSkipTypes) > 0 {
+		config.SkipType = append(config.SkipType, addSkipTypes...)
+	}
 
-	if relIndex > 0 {
-		config.RelevanceIndex = relIndex
+	for _, skipType := range deleteSkipTypes {
+		config.SkipType = removeElements(config.SkipType, skipType)
+	}
+
+	if len(addSkipFiles) > 0 {
+		config.SkipFile = append(config.SkipFile, addSkipFiles...)
+	}
+
+	for _, fileName := range deleteSkipFiles {
+		config.SkipFile = removeElements(config.SkipFile, fileName)
+	}
+
+	if relevanceIndex > 0 {
+		config.RelevanceIndex = relevanceIndex
 	}
 
 	if err := SaveConfig(config); err != nil {
@@ -54,6 +70,15 @@ func setConfig(directories []string, skipTypes []string, skipFiles []string, rel
 
 	fmt.Println("Configurations Saved Successfully")
 	return nil
+}
+
+func removeElements(slice []string, element string) []string {
+	for i, v := range slice {
+		if v == element {
+			slice = append(slice[:i], slice[i+1:]...)
+		}
+	}
+	return slice
 }
 
 func LoadConfig() (*ConfigData, error) {
@@ -95,4 +120,14 @@ func SaveConfig(config *ConfigData) error {
 
 	encoder := json.NewEncoder(file)
 	return encoder.Encode(config)
+}
+
+func showConfigFormatted(config *ConfigData) {
+	configBytes, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		fmt.Printf("Failed to format config: %v\n", err)
+		return
+	}
+	fmt.Printf("Current Configuration:\n%s\n", string(configBytes))
+
 }
