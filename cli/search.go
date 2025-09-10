299696cf5dd569baaa75c3049b51213874f1bd02
Lakshya Jain <lakshyajainrj19@gmail.com>
2025-09-10 13:26:00 +0530

    feat: Open file from search result

--- cli/search.go
+++ cli/search.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"gemini_cli_tool/fileinfo"
 	"gemini_cli_tool/gemini"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -24,6 +26,18 @@ func searchFilesCmd(cmd *cobra.Command, args []string) error {
 	}
 
 	fmt.Printf("\n%s \n\n%s %s\n\n%s %s\\%s\n\n%s %s\n", fileinfo.Green("Most relevelent file is -"), fileinfo.Yellow("File :"), file.Name, fileinfo.Yellow("File path :"), file.Directory, file.Name, fileinfo.Yellow("Description :"), file.Description)
+	fmt.Print(fileinfo.Blue("\nEnter 'y' to open this file, or any other key to cancel: "))
+
+	var response string
+	fmt.Scanln(&response)
+
+	if strings.ToLower(response) == "y" {
+		filepath := filepath.Join(file.Directory, file.Name)
+		err = OpenFileWithDefaultApp(filepath)
+		if err != nil {
+			fmt.Print(fileinfo.Red(fmt.Sprintf("Failed to open file: %v", err)))
+		}
+	}
 
 	return nil
 }
